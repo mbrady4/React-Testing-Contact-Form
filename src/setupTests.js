a0b834809c605c5ee9e97af35ebeac009ba92da6
Michael Brady <mikebrady44@gmail.com>
2020-05-15 08:29:13 -0400

    Completed Assignment

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,21 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import React from 'react';
+import ContactForm from './components/ContactForm.js';
+import * as rtl from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+
+test('renders submit button', () => {
+    const form = rtl.render(<ContactForm />);
+
+    const buttonText = form.queryByText(/message/i);
+
+    expect(buttonText).toBeInTheDocument();
+});
+
+test('Test whether input takes appropriate text', () => {
+    const form = rtl.render(<ContactForm />);
+    const input = form.getByPlaceholderText('Edd')
+    fireEvent.change(input, { target: { value: 'Michael'} })
+    expect(input.value).toBe('Michael');
+}); 
